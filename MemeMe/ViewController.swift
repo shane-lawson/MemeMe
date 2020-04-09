1c80a7577e970d3b7822ee0c25ca802f36ba1c79
Shane Lawson <shane-lawson@users.noreply.github.com>
2020-04-09 14:40:34 -0500

    Implement ImagePickerControllers for bottom toolbar buttons
    Add entries in Info.plist to access Camera and Media Library

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
 
    @IBOutlet weak var topCaptionTextField: UITextField!
    @IBOutlet weak var bottomCaptionTextField: UITextField!
@@ -20,15 +20,39 @@ class ViewController: UIViewController {
    }
 
    @IBAction func shareButtonTapped(_ sender: UIBarButtonItem) {
+      print("share button tapped")
    }
    
    @IBAction func cancelButtonTapped(_ sender: UIBarButtonItem) {
+      print("cancel button tapped")
    }
    
    @IBAction func cameraButtonTapped(_ sender: UIBarButtonItem) {
+      present(createImagePickerControllerWith(source: .camera), animated: true, completion: nil)
    }
    
    @IBAction func albumButtonTapped(_ sender: UIBarButtonItem) {
+      present(createImagePickerControllerWith(source: .photoLibrary), animated: true, completion: nil)
+   }
+   
+   fileprivate func createImagePickerControllerWith(source: UIImagePickerController.SourceType) -> UIImagePickerController {
+      let imagePickerController = UIImagePickerController()
+      imagePickerController.sourceType = source
+      imagePickerController.delegate = self
+      return imagePickerController
+   }
+   
+   // MARK: - UIImagePickerControllerDelegate
+   
+   func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+      if let image = info[.originalImage] as? UIImage {
+         imageView.image = image
+      }
+      dismiss(animated: true, completion: nil)
+   }
+   
+   func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+      dismiss(animated: true, completion: nil)
    }
 }
 
