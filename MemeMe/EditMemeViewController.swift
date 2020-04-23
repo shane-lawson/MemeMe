c9a916e6536cf94672e0da4e3a9941f07c384cc0
Shane Lawson <shane-lawson@users.noreply.github.com>
2020-04-23 16:51:55 -0500

    Add capability for captions to adapt to image
    Refactored subscribing/unsubscribing to notification flow using
    NotificationType enum

--- MemeMe/EditMemeViewController.swift
+++ MemeMe/EditMemeViewController.swift
@@ -10,19 +10,48 @@ import UIKit
 
 class EditMemeViewController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate, UITextFieldDelegate, UIFontPickerViewControllerDelegate {
    
-   enum KeyboardNotificationType {
-      case willShow
-      case willHide
+   enum NotificationType {
+      case keyboardWillShow
+      case keyboardWillHide
+      case orientationDidChange
+      
+      var notification: NSNotification.Name {
+         switch self {
+         case .keyboardWillShow:
+            return UIResponder.keyboardWillShowNotification
+         case .keyboardWillHide:
+            return UIResponder.keyboardWillHideNotification
+         case .orientationDidChange:
+            return UIDevice.orientationDidChangeNotification
+         }
+      }
+      
+      var selector: Selector {
+         switch self {
+         case .keyboardWillShow:
+            return #selector(keyboardWillShow(_:))
+         case .keyboardWillHide:
+            return #selector(keyboardWillHide(_:))
+         case .orientationDidChange:
+            return #selector(orientationDidChange(_:))
+         }
+      }
    }
+   
+   // MARK: - Properites
+   
+   var imageViewConstraints = [NSLayoutConstraint]()
    
    // MARK: - IBOutlets
    
    @IBOutlet weak var topCaption: UITextField!
    @IBOutlet weak var bottomCaption: UITextField!
    @IBOutlet weak var imageView: UIImageView!
+   @IBOutlet weak var memeView: UIView!
    @IBOutlet weak var cameraButton: UIBarButtonItem!
    @IBOutlet weak var shareButton: UIBarButtonItem!
    @IBOutlet var toolbars: [UIToolbar]!
+
    
    // MARK: - Overrides
    
@@ -30,21 +59,25 @@ class EditMemeViewController: UIViewController, UIImagePickerControllerDelegate 
       super.viewDidLoad()
       configureMemeCaption(topCaption)
       configureMemeCaption(bottomCaption)
+      
+      setConstraints()
    }
-
+   
    override func viewWillAppear(_ animated: Bool) {
       super.viewWillAppear(animated)
       cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
       shareButton.isEnabled = false
       imageView.contentMode = .scaleAspectFit
-      subscribeToKeyboardNotification(of: .willShow)
-      subscribeToKeyboardNotification(of: .willHide)
+      subscribeToNotification(of: .keyboardWillShow)
+      subscribeToNotification(of: .keyboardWillHide)
+      subscribeToNotification(of: .orientationDidChange)
    }
    
    override func viewWillDisappear(_ animated: Bool) {
       super.viewWillDisappear(animated)
-      unsubscribeFromKeyboardNotification(of: .willShow)
-      unsubscribeFromKeyboardNotification(of: .willHide)
+      unsubscribeFromNotification(of: .keyboardWillShow)
+      unsubscribeFromNotification(of: .keyboardWillHide)
+      unsubscribeFromNotification(of: .orientationDidChange)
    }
    
    //MARK: - IBActions
@@ -124,27 +157,26 @@ class EditMemeViewController: UIViewController, UIImagePickerControllerDelegate 
       textField.adjustsFontSizeToFitWidth = true
       textField.minimumFontSize = 17.0
       textField.textAlignment = .center
+      // set constraint so caption height is always the intrinsic content size
+      textField.translatesAutoresizingMaskIntoConstraints = false
+      textField.heightAnchor.constraint(equalToConstant: textField.intrinsicContentSize.height).isActive = true
    }
    
-   fileprivate func subscribeToKeyboardNotification(of type: KeyboardNotificationType) {
-      switch type {
-      case .willShow:
-         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
-      case .willHide:
-         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
-      }
+   fileprivate func subscribeToNotification(of type: NotificationType) {
+      NotificationCenter.default.addObserver(self, selector: type.selector, name: type.notification, object: nil)
    }
    
-   fileprivate func unsubscribeFromKeyboardNotification(of type: KeyboardNotificationType) {
-      switch type {
-      case .willShow:
-         NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
-      case .willHide:
-         NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
-      }
+   fileprivate func unsubscribeFromNotification(of type: NotificationType) {
+      NotificationCenter.default.removeObserver(self, name: type.notification, object: nil)
    }
    
    @objc fileprivate func keyboardWillShow(_ notification: Notification) {
+      func getKeyboardHeight(_ notification: Notification) -> CGFloat {
+         let userInfo = notification.userInfo
+         let keyboardRect = userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! CGRect
+         return keyboardRect.height
+      }
+      
       if bottomCaption.isEditing {
          view.frame.origin.y -= getKeyboardHeight(notification)
       }
@@ -155,11 +187,64 @@ class EditMemeViewController: UIViewController, UIImagePickerControllerDelegate 
          view.frame.origin.y = 0
       }
    }
+
+   @objc fileprivate func orientationDidChange(_ notification: Notification) {
+      updateImageConstraints()
+   }
+
+   fileprivate func setConstraints() {
+      imageView.translatesAutoresizingMaskIntoConstraints = false
+      // add image view to meme view and set constraints relative to meme view
+      memeView.addSubview(imageView)
+      updateImageConstraints()
+      // add top caption to meme view and set constraints relative to image view
+      memeView.addSubview(topCaption)
+      topCaption.topAnchor.constraint(equalTo: imageView.topAnchor, constant: 20).isActive = true
+      topCaption.leadingAnchor.constraint(equalTo: imageView.leadingAnchor, constant: 20).isActive = true
+      imageView.trailingAnchor.constraint(equalTo: topCaption.trailingAnchor, constant: 20).isActive = true
+      // add bottom caption to meme view and set constraints relative to image view
+      memeView.addSubview(bottomCaption)
+      imageView.bottomAnchor.constraint(equalTo: bottomCaption.bottomAnchor, constant: 20).isActive = true
+      bottomCaption.leadingAnchor.constraint(equalTo: imageView.leadingAnchor, constant: 20).isActive = true
+      imageView.trailingAnchor.constraint(equalTo: bottomCaption.trailingAnchor, constant: 20).isActive = true
+   }
    
-   fileprivate func getKeyboardHeight(_ notification: Notification) -> CGFloat {
-      let userInfo = notification.userInfo
-      let keyboardRect = userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! CGRect
-      return keyboardRect.height
+   fileprivate func updateImageConstraints() {
+      func getImageRect(rect: CGRect, image: UIImage) -> CGRect {
+         let widthFactor = rect.width / image.size.width
+         let heightFactor = rect.height / image.size.height
+         let minFactor = min(widthFactor, heightFactor)
+         
+         let size = CGSize(width: minFactor * image.size.width, height: minFactor * image.size.height)
+         let origin = CGPoint(x: (rect.width - size.width) / 2 , y: (rect.height - size.height) / 2)
+         return CGRect(origin: origin, size: size)
+      }
+      
+      imageViewConstraints.forEach { $0.isActive = false }
+      imageViewConstraints.removeAll()
+      if let image = imageView.image {
+         let imageRect = getImageRect(rect: memeView.frame, image: image)
+         if imageRect.origin.x <= imageRect.origin.y {
+            // for images that are wider than the view, pin the top and bottom to the scaled image rect, and leading and trailing to "parent" edges
+            imageViewConstraints.append(imageView.topAnchor.constraint(equalTo: memeView.topAnchor, constant: imageRect.origin.y))
+            imageViewConstraints.append(memeView.bottomAnchor.constraint(equalTo: imageView.bottomAnchor, constant: imageRect.origin.y))
+            imageViewConstraints.append(imageView.leadingAnchor.constraint(equalTo: memeView.leadingAnchor, constant: 0))
+            imageViewConstraints.append(memeView.trailingAnchor.constraint(equalTo: imageView.trailingAnchor, constant: 0))
+         } else {
+            // for images that are narrower than the view, pin the top and bottom to "parent" edges, and leading and trailing to the scaled image rect
+            imageViewConstraints.append(imageView.topAnchor.constraint(equalTo: memeView.topAnchor, constant: 0))
+            imageViewConstraints.append(memeView.bottomAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 0))
+            imageViewConstraints.append(imageView.leadingAnchor.constraint(equalTo: memeView.leadingAnchor, constant: imageRect.origin.x))
+            imageViewConstraints.append(memeView.trailingAnchor.constraint(equalTo: imageView.trailingAnchor, constant: imageRect.origin.x))
+         }
+      } else {
+         // when the constraints are initially set, pin them all to the "parent" edges
+         imageViewConstraints.append(imageView.topAnchor.constraint(equalTo: memeView.topAnchor, constant: 0))
+         imageViewConstraints.append(memeView.bottomAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 0))
+         imageViewConstraints.append(imageView.leadingAnchor.constraint(equalTo: memeView.leadingAnchor, constant: 0))
+         imageViewConstraints.append(memeView.trailingAnchor.constraint(equalTo: imageView.trailingAnchor, constant: 0))
+      }
+      imageViewConstraints.forEach { $0.isActive = true }
    }
    
    // MARK: - UIImagePickerControllerDelegate
@@ -167,6 +252,7 @@ class EditMemeViewController: UIViewController, UIImagePickerControllerDelegate 
    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
       if let image = info[.originalImage] as? UIImage {
          imageView.image = image
+         updateImageConstraints()
          shareButton.isEnabled = true
       }
       dismiss(animated: true, completion: nil)
